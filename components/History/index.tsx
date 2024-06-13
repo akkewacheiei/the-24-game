d47d36d247d98d313162a501ff3edf40d899b972
Broy <akkewach.yodsomboon@gmail.com>
2024-06-14 02:55:00 +0700

    delete import

--- components/History/index.tsx
+++ components/History/index.tsx
@@ -1,7 +1,5 @@
 "use client";
 import React, { useState, useEffect } from "react";
-import Link from "next/link";
-import Navbar from "../../components/Navbar/index";
 import axios from "axios";
 
 type HistoryItem = {
