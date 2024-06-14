1f6b113d030678221e846db5134f4a9c01e22c7e
Broy <akkewach.yodsomboon@gmail.com>
2024-06-14 22:42:41 +0700

    refactor game.tsx

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -6,7 +6,20 @@ import History from "@/components/History/index";
 import { API_BASE_URL } from "@/config";
 import { useAuth } from "@/contexts/AuthContext";
 
-export default function Game() {
+const styles = {
+  quitButton:
+    "border-[1px] shadow-lg rounded-lg bg-green-400 w-[10rem] h-[2.5rem] text-white font-bold",
+  inputBox: "border rounded p-2",
+  submitButton:
+    "ml-3 border-[1px] shadow-lg rounded-lg bg-green-400 w-[5rem] h-[2.5rem] text-white font-bold",
+  solutionButton:
+    "border-[1px] shadow-lg rounded-lg bg-green-400 w-[10rem] h-[2.5rem] text-white font-bold",
+  clearButton: "text-red-600 font-bold cursor-pointer",
+  startButton:
+    "mt-14 border-[1px] shadow-lg rounded-lg bg-green-400 w-[10rem] h-[5rem] text-white font-bold text-xl",
+};
+
+const Game = () => {
   const { authState } = useAuth();
   const { user } = authState;
 
@@ -17,10 +30,7 @@ export default function Game() {
   const [viewHistory, setViewHistory] = useState<boolean>(false);
 
   const handleStart = useCallback(async () => {
-    setSolution("");
-    setFeedback("");
-    setValidExpressions([]);
-
+    resetState();
     try {
       const response = await axios.get(`${API_BASE_URL}/generate-numbers`);
       setNumbers(response.data.numbers);
@@ -47,120 +57,115 @@ export default function Game() {
   }, [user, numbers, solution]);
 
   const handleGetSolutions = useCallback(async () => {
-    console.log("numbers:", numbers);
-
     try {
-      const response = await axios.post(`${API_BASE_URL}/cheat`, {
-        numbers,
-      });
-      console.log("handleGetSolutions res:", response);
+      const response = await axios.post(`${API_BASE_URL}/cheat`, { numbers });
       setValidExpressions(response.data.validExpressions);
     } catch (error) {
       console.error("Error cheat:", error);
     }
   }, [numbers]);
 
+  const resetState = () => {
+    setSolution("");
+    setFeedback("");
+    setValidExpressions([]);
+  };
+
+  const renderGameContent = () => {
+    if (numbers.length > 0) {
+      return (
+        <div className="flex flex-col items-center gap-3">
+          <button onClick={() => setNumbers([])} className={styles.quitButton}>
+            Quit
+          </button>
+          <div className="mt-5 text-white text-xl">
+            Generated Numbers: {numbers.join(", ")}
+          </div>
+          <div className="mt-5">
+            <input
+              type="text"
+              value={solution}
+              onChange={(e) => setSolution(e.target.value)}
+              placeholder="Enter your solution"
+              className={styles.inputBox}
+            />
+            <button
+              onClick={handleSubmitSolution}
+              className={styles.submitButton}
+            >
+              Submit
+            </button>
+          </div>
+          {feedback && (
+            <div
+              className={`mt-5 ${
+                feedback === "Correct!" ? `text-white` : `text-red-500`
+              } font-bold text-xl`}
+            >
+              {feedback}
+            </div>
+          )}
+          <div className="mt-5 flex items-center gap-6">
+            <button
+              onClick={handleGetSolutions}
+              className={styles.solutionButton}
+            >
+              Get Solutions
+            </button>
+            <p
+              className={styles.clearButton}
+              onClick={() => setValidExpressions([])}
+            >
+              Clear
+            </p>
+          </div>
+          <h2>
+            {validExpressions.length > 0 &&
+              `${validExpressions.length} solutions found`}
+          </h2>
+          <div className="flex gap-10 flex-wrap w-[20rem]">
+            {validExpressions.length > 0 &&
+              validExpressions.map((item, index) => <p key={index}>{item}</p>)}
+          </div>
+        </div>
+      );
+    } else {
+      return (
+        <div className="flex flex-col items-center gap-3">
+          <p className="text-4xl font-bold">Hello "{user?.username}"</p>
+          <p className="cursor-pointer" onClick={() => setViewHistory(true)}>
+            {"-> View history"}
+          </p>
+          <button onClick={handleStart} className={styles.startButton}>
+            Start Game
+          </button>
+        </div>
+      );
+    }
+  };
+
+  const renderHistory = () => {
+    return (
+      <>
+        <p
+          className="font-bold cursor-pointer"
+          onClick={() => setViewHistory(false)}
+        >
+          {"<- Main Page"}
+        </p>
+        <History userId={user?.id} />
+      </>
+    );
+  };
+
   return (
     <div>
       <Navbar />
       <div className="min-h-screen p-[10rem] flex flex-col items-center justify-center bg-gradient-to-r from-blue-300 to-purple-400 gap-11">
-        {!viewHistory ? (
-          <>
-            {numbers.length > 0 ? (
-              <div className="flex flex-col items-center gap-3">
-                <button
-                  onClick={() => {
-                    setNumbers([]);
-                  }}
-                  className="border-[1px] shadow-lg rounded-lg bg-green-400 w-[10rem] h-[2.5rem] text-white font-bold"
-                >
-                  {"Quit"}
-                </button>
-                <div className="mt-5 text-white text-xl">
-                  Generated Numbers: {numbers.join(", ")}
-                </div>
-                <div className="mt-5">
-                  <input
-                    type="text"
-                    value={solution}
-                    onChange={(e) => setSolution(e.target.value)}
-                    placeholder="Enter your solution"
-                    className="border rounded p-2"
-                  />
-                  <button
-                    onClick={handleSubmitSolution}
-                    className="ml-3 border-[1px] shadow-lg rounded-lg bg-green-400 w-[5rem] h-[2.5rem] text-white font-bold"
-                  >
-                    Submit
-                  </button>
-                </div>
-                {feedback && (
-                  <div
-                    className={`mt-5 ${
-                      feedback === "Correct!" ? `text-white` : `text-red-500`
-                    } font-bold  text-xl`}
-                  >
-                    {feedback}
-                  </div>
-                )}
-
-                <div className="mt-5 flex items-center gap-6 ">
-                  <button
-                    onClick={handleGetSolutions}
-                    className="border-[1px] shadow-lg rounded-lg bg-green-400 w-[10rem] h-[2.5rem] text-white font-bold"
-                  >
-                    {"Get Solutions"}
-                  </button>
-                  <p
-                    className=" text-red-600 font-bold cursor-pointer"
-                    onClick={() => setValidExpressions([])}
-                  >
-                    Clear
-                  </p>
-                </div>
-
-                <h2>
-                  {validExpressions.length > 0 &&
-                    `${validExpressions.length} solutions found`}
-                </h2>
-
-                <div className="flex gap-10 flex-wrap w-[20rem]">
-                  {validExpressions.length > 0 &&
-                    validExpressions.map((item, index) => (
-                      <p key={index}>{item}</p>
-                    ))}
-                </div>
-              </div>
-            ) : (
-              <div className="flex flex-col items-center gap-3">
-                <p className="text-4xl font-bold">Hello "{user?.username}"</p>
-                <p
-                  className="cursor-pointer"
-                  onClick={() => setViewHistory(true)}
-                >
-                  {"-> View history"}
-                </p>
-                <button
-                  onClick={handleStart}
-                  className="mt-14 border-[1px] shadow-lg rounded-lg bg-green-400 w-[10rem] h-[5rem] text-white font-bold text-xl"
-                >
-                  {"Start Game"}
-                </button>
-              </div>
-            )}
-          </>
-        ) : (
-          <>
-            <p
-              className="font-bold cursor-pointer"
-              onClick={() => setViewHistory(false)}
-            >
-              {"<- Main Page"}
-            </p>
-            <History userId={user?.id}></History>
-          </>
-        )}
+        {viewHistory ? renderHistory() : renderGameContent()}
       </div>
     </div>
   );
-}
+};
+
+export default Game;
