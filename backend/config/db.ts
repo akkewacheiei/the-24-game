1db7aa9f8e77a832a2d160ce0faf565ad5f99964
Broy <akkewach.yodsomboon@gmail.com>
2024-06-15 13:33:00 +0700

    add port:3306 in config/db.ts

--- backend/config/db.ts
+++ backend/config/db.ts
@@ -2,6 +2,7 @@ import { Sequelize } from 'sequelize-typescript';
 
 const sequelize = new Sequelize("the_24_game", "root", "root_password", {
   host: "localhost",
+  port: 3306,
   dialect: "mysql"
 });
 
