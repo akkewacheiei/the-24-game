575c35c56d543249b38ad29acc6c2b926fe5ec12
Broy <akkewach.yodsomboon@gmail.com>
2024-06-15 18:41:17 +0700

    change port mySQL

--- backend/config/db.ts
+++ backend/config/db.ts
@@ -2,7 +2,7 @@ import { Sequelize } from 'sequelize-typescript';
 
 const sequelize = new Sequelize("the_24_game", "root", "root_password", {
   host: "localhost",
-  port: 3306,
+  port: 3307,
   dialect: "mysql"
 });
 
